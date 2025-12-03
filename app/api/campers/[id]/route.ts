da892c269bbf36f84b5cc9a7ee1ba25e7b84fa66
Andrii Butenko <andrejbutik@gmail.com>
2025-12-03 15:39:34 +0100

    did route for id campers

--- app/api/campers/[id]/route.ts
+++ app/api/campers/[id]/route.ts
@@ -1,6 +1,26 @@
-import { NextRequest } from "next/server";
+import { isAxiosError } from 'axios';
+import { NextResponse } from 'next/server';
+import { api } from '../../api';
 
-export async function GET(request: NextRequest) {
-    return "";
+type Props = {
+  params: Promise<{ id: string }>;
+};
 
+export async function GET(request: Request, { params }: Props) {
+  try {
+    const { id } = await params;
+    const res = await api(`/campers/${id}`);
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
 }
